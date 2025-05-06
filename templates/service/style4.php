da1240613b4102e24287ef07e66762d3bee3bfa7
cuong <huycuongytam@gmail.com>
2025-05-06 22:35:20 +0700

    update

--- templates/service/style4.php
+++ templates/service/style4.php
@@ -1,8 +1,26 @@
 <div class="item data-<?php echo esc_attr(get_the_ID()); ?>" >				
 
-    <div class="services-post hover-flash scale-hover">
+    <div class="services-post">
 
-    <div class="featured-post">
+
+        <div class="content"> 
+
+
+        <?php if ( $settings['show_icon'] == 'yes' ): ?>
+                <?php if(!empty($icon)): ?>
+                    <div class="icon">
+                        <?php echo $icon; ?>
+                    </div>
+                <?php endif; ?>
+            <?php endif; ?>
+
+            <h5 class="title border_eff">
+
+                <a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a>
+
+            </h5>
+
+            <div class="featured-post">
 
 <a href="<?php echo get_the_permalink(); ?>">
 
@@ -18,18 +36,6 @@ echo sprintf('<img src="%s" class="lazyload" alt="image">', \Elementor\Group_Con
 
 </div>
 
-
-        <div class="content"> 
-
-
-
-
-            <h5 class="title">
-
-                <a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a>
-
-            </h5>
-
             <?php if ( $settings['show_exc'] == 'yes' ): ?>
 
                 <p class="description"><?php echo wp_trim_words( get_the_content(), $settings['excerpt_lenght'], '' ); ?></p>
@@ -43,7 +49,7 @@ echo sprintf('<img src="%s" class="lazyload" alt="image">', \Elementor\Group_Con
                 <div class="tf-button-container">
 
                 <a href="<?php echo esc_url( get_permalink() ) ?>" class="tf-button">
-                        <span><?php echo esc_attr( $settings['button_text'] ); ?></span>
+                        <i class="icon-micare-arrow-right"></i>
                     </a>
 
                 </div>
--- templates/service/style5.php
+++ templates/service/style5.php
@@ -1,54 +1,45 @@
-<?php 
-global $post;
-$id = $post->ID;
-$termsArray = get_the_terms( $id, 'services_category' );
-$termsString = "";
-if ( $termsArray ) {
-	foreach ( $termsArray as $term ) {
-		$itemname = strtolower( $term->slug ); 
-		$itemname = str_replace( ' ', '-', $itemname );
-		$termsString .= $itemname.' ';
-	}
-}
-$features = get_post_meta($post->ID, 'features_services_value', true);
-?>
-<div class="item <?php echo esc_attr( $termsString ); ?> animated fadeInUpBig">				
-    <div class="services-post hover-flash scale-hover">
+<div class="item data-<?php echo esc_attr(get_the_ID()); ?>" >				
 
+    <div class="services-post">
 
 
         <div class="content"> 
-            <h4 class="title">
+
+
+        <?php if ( $settings['show_icon'] == 'yes' ): ?>
+                <?php if(!empty($icon)): ?>
+                    <div class="icon">
+                        <?php echo $icon; ?>
+                    </div>
+                <?php endif; ?>
+            <?php endif; ?>
+
+            <h5 class="title border_eff">
+
                 <a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a>
-            </h4>
+
+            </h5>
+
+
             <?php if ( $settings['show_exc'] == 'yes' ): ?>
+
                 <p class="description"><?php echo wp_trim_words( get_the_content(), $settings['excerpt_lenght'], '' ); ?></p>
+
             <?php endif; ?>
-            <?php if ( !empty($features) ): ?>
-                <div class="group-features">
-                    <?php echo wp_kses_post($features); ?>
-                </div>
-            <?php endif; ?>
+
+
+
             <?php if ( $settings['show_button'] == 'yes' ): ?>
                 <div class="tf-button-container">
-                <a href="<?php echo esc_url( get_permalink() ) ?>" class="tf-button tf-btn">
+                    <a href="<?php echo esc_url( get_permalink() ) ?>" class="tf-button">
                         <span><?php echo esc_attr( $settings['button_text'] ); ?></span>
+                        <i class="icon-micare-arrow-right"></i>
                     </a>
                 </div>
             <?php endif; ?>
-        </div>
 
-        <div class="featured-post">
-            <a href="<?php echo get_the_permalink(); ?>">
-                <?php 
-                $get_id_post_thumbnail = get_post_thumbnail_id();
-                echo sprintf('<img src="%s" class="lazyload" alt="image">', \Elementor\Group_Control_Image_Size::get_attachment_image_src( $get_id_post_thumbnail, 'thumbnail', $settings ));
-                ?>
-            </a>
         </div>
 
     </div>
+
 </div>
-
-
-
